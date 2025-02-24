0d717f6c9bafaa2c94c7b93b2d29faafb74169f1
apelsin apelsin <abrikosechek@gmail.com>
2025-02-25 03:17:58 +0400

    table visuals

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,48 @@
   <div class="page">
     <div class="header">
       <h1 class="header__title">Учетные записи</h1>
-      <Button icon="pi pi-plus-circle" variant="outlined"  />
+      <Button icon="pi pi-plus-circle" variant="outlined" />
     </div>
+
     <Message severity="info" icon="pi pi-question-circle">
       Для указания одной или нескольких меток используйте разделитель ;
     </Message>
+
+    <div class="table">
+      <!-- Table header -->
+      <div class="table__cell">
+        <p>
+          Метки
+        </p>
+      </div>
+      <div class="table__cell">
+        <p>
+          Тип записи
+        </p>
+      </div>
+      <div class="table__cell">
+        <p>
+          Логин
+        </p>
+      </div>
+      <div class="table__cell">
+        <p>
+          Пароль
+        </p>
+      </div>
+      <!-- Empty header block -->
+      <div class="table__cell" />
+
+      <!-- Table body -->
+       <TableItem />
+       <TableItem wide />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Button, Message } from 'primevue';
+import TableItem from '@/components/TableItem.vue';
 </script>
 
 <style scoped lang="scss">
@@ -27,7 +59,21 @@ import { Button, Message } from 'primevue';
 
 .page {
   width: 100%;
-  max-width: 800px;
+  max-width: 1200px;
   margin: 0 auto;
+}
+
+.table {
+  display: grid;
+  grid-template-columns: 2fr 1.5fr 2fr 2fr auto;
+  gap: 12px;
+  width: 100%;
+
+  &__cell {
+    &>p {
+      font-size: 14px;
+      opacity: 80%;
+    }
+  }
 }
 </style>
---
+++ src/components/TableItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <InputText type="text" placeholder="Введите" />
+  <Select :options="noteTypesList" optionLabel="name" placeholder="Выберите" class="w-full md:w-56" />
+
+  <template v-if="!wide">
+    <InputText type="text" placeholder="Введите" />
+    <InputText type="text" placeholder="Введите" />
+  </template>
+  <InputText v-else class="wide" type="text" placeholder="Введите" />
+
+  <Button icon="pi pi-trash" severity="danger" variant="text" />
+</template>
+
+<script setup lang='ts'>
+import { InputText, Select, Button } from 'primevue';
+
+interface Props {
+  wide?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  wide: false
+})
+
+interface NoteTypes {
+  id: string | number
+  name: string
+};
+
+const noteTypesList: NoteTypes[] = [
+  {
+    name: "Локальная",
+    id: "local"
+  },
+  {
+    name: "LDAP",
+    id: "ldap"
+  },
+]
+</script>
+
+<style scoped lang='scss'>
+.wide {
+  grid-column: span 2;
+}
+</style>
