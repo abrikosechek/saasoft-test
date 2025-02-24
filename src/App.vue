ed9105ab0cf3911ba60e9d1d8fecaee47fda19c1
apelsin apelsin <abrikosechek@gmail.com>
2025-02-24 16:21:59 +0400

    icons

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-  <Button size="small">
-    Click Me!
-  </Button>
-  <Baba />
+  <div class="page">
+    <div class="header">
+      <h1 class="header__title">Учетные записи</h1>
+      <Button icon="pi pi-home" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Button } from 'primevue';
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  &__title {}
+}
+
+.page {
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
